ad0ccae01fed2ff4b202a0405102573c1826dcb1
javlav98 <jrlavin98@gmail.com>
2023-08-08 19:01:30 -0700

    added loader

--- Frontend/src/Components/ContactForm.js
+++ Frontend/src/Components/ContactForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import  './ContactForm.css';
-
+import './ContactForm.css';
+import { ThreeDots } from 'react-loader-spinner';
 
 const ContactForm = () => {
   const [name, setName] = useState('');
@@ -8,6 +8,7 @@ const ContactForm = () => {
   const [message, setMessage] = useState('');
   const [isSent, setIsSent] = useState(false);
   const [showError, setShowError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,6 +19,7 @@ const ContactForm = () => {
     }
 
     setShowError(false);
+    setIsLoading(true);
 
     fetch('http://localhost:5000/send-email', {
       method: 'POST',
@@ -26,41 +28,43 @@ const ContactForm = () => {
       },
       body: JSON.stringify({ name, email, message }),
     })
-
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
         setIsSent(true);
+        setIsLoading(false);
+        setName('');
+        setEmail('');
+        setMessage('');
 
+        setTimeout(() => {
+          setIsSent(false);
+        }, 5000);
       })
-
       .catch((error) => {
         console.error('Error:', error);
+        setIsLoading(false);
       });
   };
 
   return (
     <div>
+      <form onSubmit={handleSubmit}>
+        <h1>Contact me</h1>
+        <label>
+          <input placeholder= "Name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
+        <label>
+          <input placeholder="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>
+          <textarea placeholder="Message" value={message} onChange={(e) => setMessage(e.target.value)} />
+        </label>
+        <button type="submit">Send Message</button>
+      </form>
       {showError && <p>Please fill out all fields before submitting.</p>}
-      {isSent ? (
-        <p>Message sent successfully!</p>
-      ) : (
-        <form onSubmit={handleSubmit}>
-          <label>
-            Name:
-            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-          </label>
-          <label>
-            Email:
-            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-          </label>
-          <label>
-            Message:
-            <textarea value={message} onChange={(e) => setMessage(e.target.value)} />
-          </label>
-          <button type="submit">Send Message</button>
-        </form>
-      )}
+      {isLoading && <div className='loaderContainer'><ThreeDots color="black" height={15} width={50} /></div>}
+      {isSent && <p>Message sent successfully!</p>}
     </div>
   );
 };
